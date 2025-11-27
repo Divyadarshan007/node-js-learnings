fde9c298f4d45e0ca604ab56f2180a246cef054d
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-11-27 17:25:21 +0530

    did crud operation using express and mongodb

--- lecture-7-mongo-crud/config/db.js
+++ lecture-7-mongo-crud/config/db.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
+
 const MANGO_URI = "mongodb://localhost:27017/schoolDb"
 const connectDb = async () => {
     try {
-        mongoose.connection.on('connected', () => {
-            console.log('database connected successfully...');
-            
-        })
+      
         await mongoose.connect(MANGO_URI)
     } catch (err) {
         console.log(err);
     }
 }
 
-export default connectDb
+export default connectDb 
--- lecture-7-mongo-crud/index.js
+++ lecture-7-mongo-crud/index.js
@@ -1,22 +1,62 @@
-import express from 'express'
-import Student from './models/studentModels.js';
-import connectDb from './config/db.js';
-const app = express()
+import express from "express";
+import Student from "./models/studentModels.js";
+import connectDb from "./config/db.js";
+const app = express();
 const PORT = 3000;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 connectDb()
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", async (req, res) => {
+    try {
+        const students = await Student.find({})
+        res.render("index", {
+            students
+        });
+    } catch (error) {
+        console.log(error);
 
-app.post('/add-student', async(req, res)=>{
+    }
+});
+
+app.get("/delete-student", async (req, res) => {
+    const { deleteId } = req.query
+    try {
+        await Student.findByIdAndDelete(deleteId)
+        return res.redirect('/')
+    } catch (error) {
+
+    }
+});
+app.get("/edit-student", async (req, res) => {
+    const { editId } = req.query
+    try {
+        const data = await Student.findById(editId)
+        return res.render('edit', {
+            data
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+});
+app.post("/edit-student", async (req, res) => {
+    const { id, ...data } = req.body;
+    try {
+        await Student.findByIdAndUpdate(id, data)
+        return res.redirect('/')
+    } catch (error) {
+        console.log(error);
+
+    }
+});
+
+app.post("/add-student", async (req, res) => {
     const data = req.body;
-    const newStudent = new Student(data)
-    await newStudent.save()
-    res.redirect('/')
-})
+    const newStudent = new Student(data);
+    await newStudent.save();
+    res.redirect("/");
+});
 
-app.listen(PORT, ()=>{})
+app.listen(PORT, () => { });
